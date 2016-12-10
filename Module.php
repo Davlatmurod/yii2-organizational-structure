40e950e07a4a91d3d40d75f46dfa01d17112450c
kuakling <kuakling@gmail.com>
2016-12-10 05:11:36 -0500

    gii crud

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace andahrm\structure;
+
+/**
+ * structure module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'andahrm\structure\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
